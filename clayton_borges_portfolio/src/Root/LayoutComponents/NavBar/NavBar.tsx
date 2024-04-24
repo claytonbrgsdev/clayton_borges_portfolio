5063eab8ee0e553d21a177c91792db2ac6e1fb02
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-04-24 07:14:05 -0300

    General imports fixes

--- clayton_borges_portfolio/src/Root/LayoutComponents/NavBar/NavBar.tsx
+++ clayton_borges_portfolio/src/Root/LayoutComponents/NavBar/NavBar.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useAppSelector, useAppDispatch } from '../../../Config/Redux/Hooks/hooks'
-import { signOutUser } from '../../../Config/Redux/Slices/authSlice';
-import { Layout, Button, Typography, Space } from 'antd';
-import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Layout, Typography } from 'antd';
+
 
 const { Header } = Layout;
-const { Text, Link } = Typography;
+const { Link } = Typography;
 
 const NavBar: React.FC = () => {
-    const dispatch = useAppDispatch();
-    const isAuthenticated = useAppSelector(state => state.auth.authenticated);
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const handleLogout = () => {
-        dispatch(signOutUser());
-    };
-
+    
     return (
         <Header style={{ backgroundColor: '#17123f', display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', height: "94px", margin: "auto" }}>
             <Link href='/home' style={{ color: 'white', fontSize: '16px' }}>
                 {/* <img src={logo} style={{  maxHeight: "48px", maxWidth: "64px", marginLeft: "17px", paddingRight: 3 }} /> */}
             </Link>
-            {isAuthenticated && (
-                <Space>
-                    <Button type="text" onClick={handleLogout}>
-                        Sair da Conta
-                    </Button>
-                </Space>
-            )}
         </Header>
     );
 };
