b49701a20037836e2eee20dbefc3a1f703755dc8
claytonbrgs <142549801+claytonbrgs@users.noreply.github.com>
2024-04-24 06:16:38 -0300

    Included redux persist in root component

--- clayton_borges_portfolio/src/Root/Main/main.tsx
+++ clayton_borges_portfolio/src/Root/Main/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from '../App/App.tsx'
-import './index.css'
+import React from 'react';
+import './index.css';
+import { Provider } from 'react-redux';
+import { store, persistor } from '../../Redux/Store/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from '../App/App';
+
+
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </React.StrictMode>,
+  );
+}
